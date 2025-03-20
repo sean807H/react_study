2eb074017acb33c8fc124c03882bc4425847182e
sean807H <ksyssyy070814@gmail.com>
2025-03-20 19:33:37 +0900

    객체 비구조화 할당

---
+++ src/UserInfo.jsx
@@ -0,0 +1,14 @@
+// function UserInfo(props){
+//     return <div>
+//         name: {PropStudy.name} age:{props.age}
+//     </div>
+// }
+
+// 파라미터에서 "객체 비구조화 할당"을 수행
+function UserInfo({name, age}){
+    return <div>
+        name: {PropStudy.name} age:{props.age}
+    </div>
+}
+
+export default UserInfo
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client' 
 // import App from './App.jsx'
 import PropStudy from './PropStudy.jsx'
+import UserInfo from './UserInfo.jsx'
 
 const root = createRoot(document.getElementById('root'))
 // root.render(<App />)
-root.render(<PropStudy hello="world" mynum = {100} obj={[1, 2, 3]}/>)
+// root.render(<PropStudy hello="world" mynum = {100} obj={[1, 2, 3]}/>)
+root.render(<UserInfo name="John" age={20}/>)
